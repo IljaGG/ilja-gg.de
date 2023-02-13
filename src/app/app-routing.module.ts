40a8a264b7c7fca47230f5cc058061ec479ea870
Ilja Gaus-Gerbeth <ilja@Iljas-MacBook-Air.local>
2023-02-13 14:29:08 +0100

    new routes in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { StartPageComponent } from './start-page/start-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: StartPageComponent },
+  { path: 'impressum', component: ImpressumComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
